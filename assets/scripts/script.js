5eaf965433c47c632be8d336bb27b3ab781650d4
alaksandar_km <ebenzomarket@gmail.com>
2021-01-17 17:56:55 +0100

    fix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,36 +12,9 @@ function showList(e) {
     console.log(h2);
 
     for(div of showHideEl) {
-        // if(h2.classList.contains("active") &&
         if(h2.parentElement.contains(div)) {
             console.log(div);
             div.classList.toggle("hide")
         }
     }
-    
-
-    // if(h2.classList.contains("active")) {
-    //     for(div of showHideEl) {
-    //         if(h2.parentElement.contains(div)) {
-    //             console.log(div);
-    //         }
-    //     }
-    // }
-            //     console.log(div);
-            //     div.classList.toggle("hide");
-            // }
-        // console.log(h2.parentElement > showHideEl);
-
-        // for (let div of showHideEl) {
-        //     console.log(div);
-        //     div.classList.toggle("hide");
-        // }
-        
-            // for (block of blocks) {
-            //     // console.log(block);
-            //     let dataClass = block.dataset.action;    
-            //     if (!dataClass) return;                                    
-            //     
-            // }  
-    
 }
